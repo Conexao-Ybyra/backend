98d4d2831c20d9c96ba29b9881412f5a220fe915
Leandro Andrade <andradeleandro.contato@gmail.com>
2023-06-28 11:24:14 -0300

    Task 3 - Model

---
+++ src/main/java/com/conexaoybyra/ConexaoybyraApplication.java
@@ -0,0 +1,13 @@
+package com.conexaoybyra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexaoybyraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexaoybyraApplication.class, args);
+	}
+
+}
